bbce78a8f8a77d3561fc20364c2b99a5866679f9
saumya <2saumyaray@gmail.com>
2020-10-27 22:48:57 +0530

    wip: making the portfolio page

--- libs/js/app.js
+++ libs/js/app.js
@@ -6,12 +6,22 @@
 // JQuery: Ready : https://api.jquery.com/ready/
 $(function(){
   console.log('ready!');
-  //
+  // index.html
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function(){
     // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
   })
-  //
+  // index.html /
+  // Portfolio.html
+  $("#btn_left").on('click', function(){
+    console.log('Left');
+    return false;
+  });
+  $("#btn_right").on('click', function(){
+    console.log('Right');
+    return false;
+  });
+  // Portfolio.html /
 })
